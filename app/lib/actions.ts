451aece7ab79f2c856e41f044998c06e737a0df2
“IM <dominiqueue7@gmail.com>
2024-04-13 19:01:15 +0900

    restore code

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -38,7 +38,24 @@ export async function createInvoice(prevState:State, formData: FormData) {
         message: 'Missing Fields. Failed to Create Invoice.',
       };
     }
+
+    // Prepare data for insertion into the database
+    const { customerId, amount, status } = validatedFields.data;
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
   
+    // Insert data into the database
+    try {
+      await sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+      `;
+    } catch (error) {
+      // If a database error occurs, return a more specific error.
+      return {
+        message: 'Database Error: Failed to Create Invoice.',
+      };
+    }
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
