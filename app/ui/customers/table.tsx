b855dfc73b4c4c201d18a32ac38ac9ae2cf2f141
“IM <dominiqueue7@gmail.com>
2024-04-14 20:09:09 +0900

    customer page

--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -5,6 +5,7 @@ import {
   CustomersTableType,
   FormattedCustomersTable,
 } from '@/app/lib/definitions';
+import useSWR from 'swr';
 
 export default async function CustomersTable({
   customers,
@@ -13,10 +14,6 @@ export default async function CustomersTable({
 }) {
   return (
     <div className="w-full">
-      <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
-        Customers
-      </h1>
-      <Search placeholder="Search customers..." />
       <div className="mt-6 flow-root">
         <div className="overflow-x-auto">
           <div className="inline-block min-w-full align-middle">
